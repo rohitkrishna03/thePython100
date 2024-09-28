bf5cfbfc6ac6cf5c91aaf957610feed3f898f271
rohitkrishna03 <rohitkdms03@gmail.com>
2024-09-28 11:59:34 +0530

    py-3

---
+++ python_10.py
@@ -0,0 +1,17 @@
+# here we learn about the boops 
+# for the loops we can also adda conditions like 
+# i<=3 only print till i is less than equal to 3
+
+i =0
+while(i<=3):
+    print(i)
+    i = i+1
+    
+print("done with the loop")
+
+# always check the identation if its shows any errors.
+# using count
+count =5
+while(count >0):
+    print(count)
+    count = count -1
---
+++ python_9.py
@@ -0,0 +1,28 @@
+# #  for loop in python 
+# # for loop can iterate over a sequence of iterable objects in python.
+# # iterating over a sequence is nothing but iterating over strings, lists, tuples, sets and dictionaries.
+# name = 'rohitkrishna'
+# for i in name:
+#     print(i)
+
+# colors = ["red", "blue", "green", "orange"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+        
+# range function
+
+for k in range(5):
+    print(k +1)
+    
+    
+for k in range(1, 9):
+    print(k)
+    # the above value prints till 8 
+    
+for k in range(1, 15, 5):
+    print(k)
+    # the above code prints after every 5 digits 
+    # then the output will be 6, 11
+    
