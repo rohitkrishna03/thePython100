e6316064343620635f8e0e163eb30283d253858f
rohitkrishna03 <rohitkdms03@gmail.com>
2024-10-15 15:03:48 +0530

    magical method

---
+++ python_67.py
@@ -0,0 +1,12 @@
+# magic and dunder(double underscore) method
+# these are special methods that you can define in your classes and when invoked, they give you a powerful way to manipulate objects and their behaviour.
+# 
+# magic methods also known as "dunder" from the double underscore surroundinf their names are powerful tools that allows you to customize
+# the behavior of your classes. they are used to implement special methods such as the addition subtraction and comparision operator, as well as some more advanced techniques like description and properties.
+# lets take a look of some of the most commonly used magical methods in python.
+# 
+# the __init__ method is the specia method that is automatically involved when you create a mew isntance of a class.
+# this method is  responsible for setting up the objects initial state, and it is where you would typically define any instance variable that you need. 
+# also called "constructor", in file python_66.py
+
+
